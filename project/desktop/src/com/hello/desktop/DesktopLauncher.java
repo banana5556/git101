e8867d7760067401b4a0d044cae4ea2fee070ebb
banana5556 <fruitbanana666@gmail.com>
2016-11-01 21:01:34 +0700

    PROJECT

---
+++ project/desktop/src/com/hello/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.hello.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hello.Hello;
+
+public class DesktopLauncher {
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		new LwjglApplication(new Hello(), config);
+	}
+}
